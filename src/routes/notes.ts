4ce544ccbed25990d7339120ae18e34a65babb59
Nando San Miguel <mythemc@gmail.com>
2022-10-08 21:41:22 -0500

    feat: add endpoint to get one note

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -7,6 +7,12 @@ router.get('/', (_req, res) => {
   res.send(notesServices.getNotes())
 })
 
+router.get('/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const note = notesServices.getNotesById(id)
+  return note != null ? res.send(note) : res.sendStatus(404)
+})
+
 router.post('/', (_req, res) => {
   res.send('note saved')
 })
--- src/services/notes.ts
+++ src/services/notes.ts
@@ -4,4 +4,9 @@ import notesData from './notesData.json'
 const notes: Note[] = notesData
 export const getNotes = (): Note[] => notes
 
+export const getNotesById = (id: number): Note | undefined => {
+  const entry = notes.find((note) => note.id === id)
+  return entry
+}
+
 export const addNotes = (): null => null
